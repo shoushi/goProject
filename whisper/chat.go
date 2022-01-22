2cb62c78f73d637de53a59a81ea99840cd68f858
guanliqun1 <guanliqun@geekplus.com>
2022-01-22 17:28:28 +0800

    聊天框修改

--- whisper/chat.go
+++ whisper/chat.go
@@ -1,15 +1,28 @@
 package whisper
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func Chat(app fyne.App, name string) {
 	chatWin := app.NewWindow(name)
-	lab := "lao ke with name " + name
-	chatlab := widget.NewLabel(lab)
-	chatWin.SetContent(chatlab)
+	record := widget.NewRichText()
+
+	inputArea := widget.NewMultiLineEntry()
+	inputAreaBut := widget.NewButton("submit", func() {
+		sentence := inputArea.Text
+		log.Println(sentence)
+		inputArea.Text = ""
+		inputArea.Refresh()
+		record.ParseMarkdown(sentence)
+	})
+	// tabs := container.NewAppTabs(container.NewTabItem("Tab 1", widget.NewLabel("Hello!")), container.NewTabItem("Tab 1", widget.NewLabel("World!")))
+	chaterWidget := container.NewGridWithRows(3, record, inputArea, inputAreaBut)
+	chatWin.SetContent(chaterWidget)
 	chatWin.Resize(fyne.NewSize(300, 200))
 	chatWin.Show()
 }
